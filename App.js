e10df070d748b9419f203eb8c7efbb41ae5d81ef
Austin Miller <austinmiller360@gmail.com>
2021-10-10 21:41:27 -0700

    Implement React-Navigation

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View } from "react-native";
 
 import { store } from "./store.js";
 import HomeScreen from "./screens/HomeScreen.js";
+import MapScreen from "./screens/MapScreen.js";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements/dist/icons/Icon";
+import { useNavigation } from "@react-navigation/core";
 
 const data = [
   {
@@ -14,16 +15,20 @@ const data = [
     id: "456",
     title: "Order food",
     image: "https://links.papareact.com/28w",
-    screen: "MapScreen",
+    screen: "EatsScreen",
   },
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return <FlatList data={data} 
   horizontal
   keyExtractor={(item) => item.id}
   renderItem={({ item }) => (
-      <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} >
+      <TouchableOpacity 
+      onPress={() => {navigation.navigate(item.screen)}}
+      style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} >
           <View>
             <Image style={{ height: 120, width: 120, resizeMode: "contain"}} source={{uri: item.image}}/>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
