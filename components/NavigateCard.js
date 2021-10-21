f2ebe842faf6a5b00af3d8f8b8cf35d99f1249cd
Austin Miller <austinmiller360@gmail.com>
2021-10-20 21:15:16 -0700

    Implement Navigate Card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import tw from 'tailwind-react-native-classnames'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/core";
 
 const NavigateCard = () => {
-    return (
-        <SafeAreaView style={tw`bg-white flex-1`}>
-            <Text style={tw`text-center py-5 text-xl`}>Good Afternoon, Austin</Text>
-        </SafeAreaView>
-    )
-}
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-export default NavigateCard
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Afternoon, Austin</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <GooglePlacesAutocomplete
+          styles={inputGooglePlacesStyle}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          onPress={(data, details = null) => {
+            console.log(details.geometry.location);
+            dispatch(
+              setDestination({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            navigation.navigate('RideOptionsCard');
+          }}
+          fetchDetails={true}
+          minLength={2}
+          enablePoweredByContainer={false}
+          placeholder="Where To?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default NavigateCard;
+
+const inputGooglePlacesStyle = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  }
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import tw from 'tailwind-react-native-classnames'
-import Map from '../components/Map'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
-    return (
-        <View>
-            <View style={tw`h-1/2 bg-red-100`}><Map /></View>
-            <View style={tw`h-1/2 bg-blue-100`}></View>
-        </View>
-    )
-}
+  const InnerStack = createNativeStackNavigator();
 
-export default MapScreen
+  return (
+    <View>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}>
+        <InnerStack.Navigator>
+          <InnerStack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{ headerShown: false }}
+          />
+          <InnerStack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{ headerShown: false }}
+          />
+        </InnerStack.Navigator>
+      </View>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default MapScreen;
+
+const styles = StyleSheet.create({});
